106dee0ba5d26802b2b39a0f18d0e0bf0e1cea37
Nitesh Kumar <nitesh841227@gmail.com>
2026-08-18 17:16:02 +0530

    ✅ Bubble Sort | Easy | C++ | Runtime: N/A | Memory: N/A | 2026-08-18

---
+++ gfg/easy/bubble-sort/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    void bubbleSort(vector<int>& arr) {
+        // code here
+        int n=arr.size();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n-i-1;j++)
+            {
+                if(arr[j]>arr[j+1])
+                {
+                    int temp=arr[j+1];
+                    arr[j+1]=arr[j];
+                    arr[j]=temp;
+                }
+            }
+        }
+        
+    }
+};
